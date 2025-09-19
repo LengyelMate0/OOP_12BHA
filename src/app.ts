9d78121f36934d4596f0df5c7c76d89af3ff7dc3
LengyelMate <lengyelmate22@gmail.com>
2025-09-19 14:21:22 +0200

    class

--- src/app.ts
+++ src/app.ts
@@ -1,44 +1,82 @@
-function generateNumbers(count: number, start: number, end: number): number[]{
-    const numbers: number[] = []
-    for(let i = 0; i < count; i++){
-        numbers.push(Math.floor(Math.random()*end) + start);
+class NumberHandler {
+
+    private _start: number;
+    private _end: number;
+    private _count: number;
+    private _numbers: number[];
+    private _statistic: Record<number,number>;
+
+    constructor(start: number, end: number, count: number) {
+        this._start = start;
+        this._end = end;
+        this._count = count;
+        this._numbers = this.generateNumbers()
+        this._statistic = {};
     }
-    return numbers;
-};
 
-const generateNumbers2 = (count: number, start: number, end: number): number[] =>(
-    Array.from({length: count}, item => Math.floor(Math.random()*end) + start)
-);
+    private generateNumbers(): number[] {
+        const ns: number[] = []
+        for (let i = 0; i < this._count; i++) {
+            ns.push(Math.floor(Math.random() * this._end) + this._start);
+        }
+        return ns;
+    };
 
-const array = generateNumbers2(30,1,5);
-console.log(array.join(", ")); 
+    const generateNumbers2 = (count: number, start: number, end: number): number[] => (
+        Array.from({ length: count }, item => Math.floor(Math.random() * end) + start)
+    );
 
-function getStatistic(a: Array<number>): Record<number, number>{
-    const statistic : Record<number, number> = {};
-    for (const key of array){
-    /* Az adott kulcs benne van-e az objektumban (rekordban)? 
-    if (key in statistic){
-        statistic[key]! ++;
-    } else{
-        statistic[key] = 1;
-    } */
-   statistic[key] = (statistic[key] ?? 0) + 1;  
+    //const array = generateNumbers2(30, 1, 5);
+    //console.log(array.join(", "));
+
+    getStatistic(a: Array<number>): Record<number, number> {
+        const statistic: Record<number, number> = {};
+        for (const key of this._numbers) {
+            /* Az adott kulcs benne van-e az objektumban (rekordban)? 
+            if (key in statistic){
+                statistic[key]! ++;
+            } else{
+                statistic[key] = 1;
+            } */
+            statistic[key] = (statistic[key] ?? 0) + 1;
+        }
+        return statistic;
     }
-    return statistic;
+
+    //const statistic = getStatistic(array)
+
+   /*  for (let key in statistic) {
+        console.log(`${key} szám: ${statistic[key]} db`);
+    };
+ */
+
+    print = () => {
+      for (let key in this._statistic) console.log(`${key} szám: ${this._statistic[key]} db`);
+    }
+
+    getSum(): number {
+        /* let sum = 0;
+        for (let key of statistic){
+            sum += key;
+        return sum;
+        } */
+        return this._numbers.reduce((summa, item) => summa + item, 0)
+    };
+
+    getFilteredNums(attr: number): number[] {
+        return this._numbers.filter(item => item % attr === 0)
+    }
+
+    getOrderedNum(value: number): number {
+        return this._numbers.findIndex(item => item === value) + 1;
+    };
+
+    getOrderedNum2(value: number): number {
+        let i = 0;
+        while (i < this._numbers.length && this._numbers[i] != value) {
+            i++;
+        }
+        return i + 1;
+    };
 }
 
-const statistic = getStatistic(array)
-
-for (let key in statistic){
-    console.log(`${key} szám: ${statistic[key]} db`);
-};
-
-function arraySum(a: number[]): number{
-    /* let sum = 0;
-    for (let key of statistic){
-        sum += key;
-    return sum;
-    } */
-   return a.reduce((summa, item) => summa + item, 0)
-};
-console.log(arraySum(array));
